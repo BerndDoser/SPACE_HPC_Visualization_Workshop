03e34ee3323484c25900067f6687c8c8fb545ec6
Bernd Doser <bernd.doser@h-its.org>
2025-04-22 12:57:09 +0200

    add illustris configuration and update ONNX export path

--- get_illustris_onnx.py
+++ get_illustris_onnx.py
@@ -9,7 +9,7 @@ artifact.download()
 export_onnx(
     ckpt_file="artifacts/model-8n67t45m:v0/model.ckpt",
     model_file="configs/spherinator/illustris.yaml",
-    export_path="data/illustris/models",
+    export_path="data/illustris/models/full_trained",
     input_shape=(2, 3, 128, 128),
     latent_shape=(2, 3),
 )
